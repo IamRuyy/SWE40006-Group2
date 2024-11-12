b53ffe430780f786060b47190871f3518cbd56d7
IamRuyy <duysin41@gmail.com>
2024-11-12 22:29:16 +0700

    test modify

--- vip_member.php
+++ vip_member.php
@@ -8,7 +8,7 @@
 <body>
 <h1>VIP Member Home Page</h1>
     <ul>
-        <li><a href="member_add_form.php">Add New Member Forms</a></li>
+        <li><a href="member_add_form.php">Add New Member Form</a></li>
         <li><a href="member_display.php">Display All Members Page</a></li>
         <li><a href="member_search.php">Search Member Page</a></li>
     </ul>
